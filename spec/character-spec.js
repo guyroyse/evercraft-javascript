9ed15061249eaa7b4814fa1cc594605a51e1a8ce
Guy Royse <guy@guyroyse.com>
2015-05-01 11:54:36 -0400

    alignments validate

--- spec/character-spec.js
+++ spec/character-spec.js
@@ -30,6 +30,22 @@ describe("Character", function() {
       expect(subject.alignment()).toBe("GOOD");
     });
 
+    it("can be changed to NEUTRAL", function() {
+      subject.alignment("NEUTRAL");
+      expect(subject.alignment()).toBe("NEUTRAL");
+    });
+
+    it("can be changed to EVIL", function() {
+      subject.alignment("EVIL");
+      expect(subject.alignment()).toBe("EVIL");
+    });
+
+    it("complains when changed to in invalid value", function() {
+      expect(function() {
+        subject.alignment("HAPPY")
+      }).toThrow("Alignment can only be GOOD, NEUTRAL, or EVIL");
+    });
+
   });
 
 });
--- src/character.js
+++ src/character.js
@@ -12,7 +12,11 @@ Evercraft.Character = {
     }
 
     function alignment(val) {
-      if (val !== undefined) _alignment = val;
+      if (val !== undefined) {
+        if (["GOOD", "NEUTRAL", "EVIL"].indexOf(val) === -1)
+          throw "Alignment can only be GOOD, NEUTRAL, or EVIL";
+        _alignment = val;
+      }
       return _alignment;
     }
 
